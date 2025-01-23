1f2e8ee93e5daaf4e3f3f23a1c11a0e179b98f12
Artemiy Martyanov <artemiy@martyanov.xyz>
2025-01-24 01:18:34 +0300

    Project setup

--- composeApp/src/commonMain/kotlin/xyz/martyanov/chess/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package xyz.martyanov.chess
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/xyz/martyanov/chess/Platform.kt
+++
@@ -1,7 +0,0 @@
-package xyz.martyanov.chess
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
